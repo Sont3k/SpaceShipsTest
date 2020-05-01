a5895db8d256c86005fbde1fcf860d416cdaf35a
Sont3k <markus.residence@gmail.com>
2020-05-01 13:22:05 +0300

    init

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    private IWeapon weapon { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        IWeapon weapon = (IWeapon)GetComponent(typeof(IWeapon));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
