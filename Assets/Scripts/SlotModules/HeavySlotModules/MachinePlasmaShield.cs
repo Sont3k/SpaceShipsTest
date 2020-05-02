f035913b916a4e4312d80a96a20ae1f6ae709862
Sont3k <markus.residence@gmail.com>
2020-05-02 15:54:25 +0300

    Created MachinePlasmaShield

---
+++ Assets/Scripts/SlotModules/HeavySlotModules/MachinePlasmaShield.cs
@@ -0,0 +1,33 @@
+using Assets.Scripts.SlotModules.GunParticles;
+using Assets.Scripts.SlotModules.LightSlotModules;
+using Assets.Scripts.SlotModules.MediumSlotModules;
+using Assets.Scripts.Slots;
+using UnityEngine;
+
+namespace Assets.Scripts.SlotModules.HeavySlotModules
+{
+    public class MachinePlasmaShield : HeavySlot
+    {
+        public override int Health { get; set; }
+
+        private MachineGun machineGun = new MachineGun(new Bullets());
+        private PlasmaCannon plasmaCannon = new PlasmaCannon(new Plasma());
+        private EnergyShield energyShield = new EnergyShield();
+
+        public override void Action()
+        {
+            if (Health > 0)
+            {
+                Debug.Log("Machine gun is shooting.");
+                Debug.Log("Plasma cannon is shooting.");
+                Debug.Log("Energy shield is on.");
+            }
+            else
+            {
+                Debug.Log("Can't shoot from machine gun, it's broken.");
+                Debug.Log("Can't shoot from plasma cannon, it's broken.");
+                Debug.Log("Can't enable energy shield, it has no energy.");
+            }
+        }
+    }
+}
