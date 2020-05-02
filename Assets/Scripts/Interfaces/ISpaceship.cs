cc22cd5ccde4441ef7e5a3372530814b1c10776d
Sont3k <markus.residence@gmail.com>
2020-05-02 21:36:13 +0300

    Implemented level logic in Spaceship

--- Assets/Scripts/Interfaces/ISpaceship.cs
+++ Assets/Scripts/Interfaces/ISpaceship.cs
@@ -5,7 +5,8 @@ namespace Assets.Scripts.Interfaces
     public interface ISpaceship
     {
         int Health { get; set; }
-        
+        int Level { get; set; }
+
         LightSlot LightSlot_1 { get; set; }
         LightSlot LightSlot_2 { get; set; }
     }
--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -16,15 +16,18 @@ namespace Assets.Scripts
         [Header("Heavy Slots")]
         [SerializeField] private HeavySlot _heavySlot;
 
-        // Weapon properties
+        // Slot properties
         public LightSlot LightSlot_1 { get => _lightSlot_1; set { _lightSlot_1 = value; } }
         public LightSlot LightSlot_2 { get => _lightSlot_2; set { _lightSlot_2 = value; } }
         public MediumSlot MediumSlot { get => _mediumSlot; set { _mediumSlot = value; } }
         public HeavySlot HeavySlot { get => _heavySlot; set { _heavySlot = value; } }
 
-        [Header("Health")]
+        [Header("Specifications")]
         [SerializeField] private int _health;
+        [Range(1, 10)][SerializeField] private int _level;
+        
         public int Health { get => _health; set { _health = value; } }
+        public int Level { get => _level; set { _level = value; } }
 
         // Update is called once per frame
         void Update()
